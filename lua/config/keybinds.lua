cfffabd2b0f7cc4c5e9b25df51765dd4e0c5b546
alfrh02 <alfred.n.hall@gmail.com>
2024-12-01 20:31:37 +0000

    ctrl z ctrl shift z

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -2,7 +2,12 @@ require("config.utils")
 
 -- misc
 nmap('<C-s>',   "<cmd>StripWhitespace<cr><cmd>:w<cr>", {}) -- ctrl S, strip whitespace & save
+imap('<C-s>',   "<cmd>StripWhitespace<cr><cmd>:w<cr>", {}) -- ctrl S, strip whitespace & save
 nmap('<C-q>',   "<cmd>:q<cr><cr>", {})                     -- ctrl Q, quit
+nmap('<C-z>',   "<cmd>:u<cr>", {})                         -- ctrl Z, undo
+imap('<C-z>',   "<cmd>:u<cr>", {})                         -- ctrl Z, undo
+nmap('<C-S-z>', "<C-r>", {})                               -- ctrl shift Z, redo
+imap('<C-S-z>', "<C-r>", {})                               -- ctrl shift Z, redo
 nmap('<C-n>',   "<cmd>set nu!<cr>")
 imap('<A-3>',   "#")                                       -- for macOS
 vmap('<Tab>',   "<C->>gv")
