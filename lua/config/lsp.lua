166620ba43fa23a46fae60bc1ad2c2d0633ea7ec
aphd <aphidarium@proton.me>
2025-08-26 16:54:36 +0100

    lsp off for now

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,12 +1,12 @@
-vim.lsp.enable({
-  "gdscript",
-  "gdshader"
-})
-
-local capabilities = require('blink.cmp').get_lsp_capabilities()
-require("lspconfig").gdscript.setup(capabilities);
-
-if vim.fn.filereadable(vim.fn.getcwd() .. "/project.godot") == 1 then
-  local addr = "./godot.pipe"
-  vim.fn.serverstart(addr);
-end
+-- vim.lsp.enable({
+--   "gdscript",
+--   "gdshader"
+-- })
+--
+-- local capabilities = require('blink.cmp').get_lsp_capabilities()
+-- require("lspconfig").gdscript.setup(capabilities);
+--
+-- if vim.fn.filereadable(vim.fn.getcwd() .. "/project.godot") == 1 then
+--   local addr = "./godot.pipe"
+--   vim.fn.serverstart(addr);
+-- end
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,11 @@
-return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    { "williamboman/mason.nvim", config=true },
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    { "j-hui/fidget.nvim", opts = {} },
-    { "folke/neodev.nvim", opts = {} },
-  }
-}
+return {}
+-- return {
+--   "neovim/nvim-lspconfig",
+--   dependencies = {
+--     { "williamboman/mason.nvim", config=true },
+--     "williamboman/mason-lspconfig.nvim",
+--     "WhoIsSethDaniel/mason-tool-installer.nvim",
+--     { "j-hui/fidget.nvim", opts = {} },
+--     { "folke/neodev.nvim", opts = {} },
+--   }
+-- }
